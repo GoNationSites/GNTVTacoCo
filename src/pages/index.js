038a5e855f326fd8508a64af0d5db718916d578d
Eric Smith <ericsmith.100@gmail.com>
2019-09-09 11:38:01 -0400

    Request data and add gnid env var

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,58 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React, { useState, useEffect } from "react"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import axios from "axios-jsonp"
+import jsonAdapter from "axios-jsonp"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const [menuData, setMenuData] = useState({})
+  const [eventData, setEventData] = useState({})
+  const [recurringData, setRecurringEventData] = useState({})
+  const gonationID = process.env.GONATIONID
+
+  // Make request for menu data
+  // todo account for > 1 powered lists / dynamic
+  const requestMenuData = id => {
+    axios({
+      url: `https://data.prod.gonation.com/pl/get?profile_id=${id}`,
+      adapter: jsonAdapter,
+    }).then(res => {
+      setMenuData(res.data[0])
+      console.log("menu datares: ", res.data[0])
+    })
+  }
+
+  const requestEventData = id => {
+    axios({
+      url: `https://data.prod.gonation.com/profile/events?profile_id=${id}`,
+      adapter: jsonAdapter,
+    }).then(res => {
+      setEventData(res)
+      console.log("regular event res: ", res)
+    })
+  }
+
+  const requestRecurringEventData = id => {
+    axios({
+      url: `https://data.prod.gonation.com/profile/recurringevents?profile_id=${id}`,
+      adapter: jsonAdapter,
+    }).then(res => {
+      setRecurringEventData(res)
+      console.log("recurring res: ", res)
+    })
+  }
+
+  useEffect(() => {
+    requestMenuData(gonationID)
+    requestEventData(gonationID)
+    requestRecurringEventData(gonationID)
+  }, [])
+
+  return (
+    <Layout>
+      <h1>GONATION TV</h1>
+      <h2>{menuData.name}</h2>
+    </Layout>
+  )
+}
 
 export default IndexPage
